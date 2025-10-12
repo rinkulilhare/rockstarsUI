b7e282c8212a038f5a2c71d816755c9e3e4e92d7
rinkulilhare <rinkulilhare@gmail.com>
2025-10-13 00:08:16 +0530

    Fix Bug: CreateEvent

--- src/app/services/event-service.ts
+++ src/app/services/event-service.ts
@@ -1,19 +1,27 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import baseUrl from './helper';
 import { Observable } from 'rxjs';
 import { text } from 'node:stream/consumers';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
-  
-  constructor(private http:HttpClient){}
+  private isBrowser:boolean;
+
+  constructor(private http:HttpClient, @Inject(PLATFORM_ID) private platformId:Object){
+    this.isBrowser=isPlatformBrowser(this.platformId);
+  }
 
   // create Event
-  public createEvent(event:any){
-    return this.http.post(`${baseUrl}/event/create`,event,{responseType:'text'});
+  public createEvent(event:any):Observable<any>{
+    const token=localStorage.getItem('jwtToken');
+    const headers = new HttpHeaders({
+      'Authorization':`Bearer ${token}`
+    })
+    return this.http.post(`${baseUrl}/event/create`,event,{headers,responseType:'text'});
   }
 
   // show Event
